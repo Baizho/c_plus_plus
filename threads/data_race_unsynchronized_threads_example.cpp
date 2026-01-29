fdd0ebe861c030c52a329cd5e265105d41a783f2
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2026-01-29 23:47:34 +0900

    Add data race example with unsynchronized counter
    Introduced a global counter and an increment function to demonstrate data races with unsynchronized threads. Added threads that increment the shared counter and print its value before and after joining, illustrating the effects of concurrent access without synchronization.

--- threads/data_race_unsynchronized_threads_example.cpp
+++ threads/data_race_unsynchronized_threads_example.cpp
@@ -9,8 +9,17 @@ void print(std::string str) {
     }
 }
 
+int counter = 0;
+
+void increment() {
+    for (int i = 0; i < 100000; ++ i) {
+        counter += 1;
+    }
+}
+
 int main() {
     // Unsynchronized threads which make conflicting accesses
+    // threads get interrupted and continue
     std::thread t1(print, "abc");
     std::thread t2(print, "def");
     std::thread t3(print, "xyz");
@@ -18,4 +27,21 @@ int main() {
     t1.join();
     t2.join();
     t3.join();
+
+    // another example, but using global shared variable
+    std::thread t4(increment);
+    std::thread t5(increment);
+    std::thread t6(increment);
+
+    // here, of course we don't expect 300000
+    std::cout << counter << "\n";
+
+    t4.join();
+    t5.join();
+    t6.join();
+    
+    // here, we also don't get 300000 because of data race
+    // some threads when resuming execution might not get latest information about counter
+    std::cout << counter << "\n";
+
 }
