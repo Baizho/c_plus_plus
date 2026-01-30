4462c11b183fb52ec596190b254e362b9f9219b2
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2026-01-30 15:31:10 +0900

    Add mutex synchronization thread example in C++
    Introduces a C++ example demonstrating thread synchronization using std::mutex. The code shows how to protect shared resources (cout and a counter variable) from data races in multithreaded scenarios.

---
+++ threads/mutex_class_synchronize_threads_example.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <mutex>
+
+std::mutex print_mutex;
+
+void print(std::string str) {
+    // we can lock the thread here if we want the whole thing to be in order
+    // print_mutex.lock();
+    for (int i = 0; i < 3; ++ i) {
+        // shared memory is from cout
+        print_mutex.lock(); 
+        std::cout << str[0] << str[1] << str[2] << "\n";
+        print_mutex.unlock();
+    }
+    // print_mutex.unlock();
+}
+
+int counter = 0;
+
+void increment() {
+    // print_mutex.lock(); // we can lock it here if we want the whole thing to be in order
+    for (int i = 0; i < 100000; ++ i) {
+        print_mutex.lock();
+        counter += 1;
+        print_mutex.unlock();
+    }
+    // print_mutex.unlock();
+}
+
+int main() {
+    // Unsynchronized threads which make conflicting accesses
+    // threads get interrupted and continue
+    std::thread t1(print, "abc");
+    std::thread t2(print, "def");
+    std::thread t3(print, "xyz");
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    // another example, but using global shared variable
+    std::thread t4(increment);
+    std::thread t5(increment);
+    std::thread t6(increment);
+
+    // here, of course we don't expect 300000
+    std::cout << counter << "\n";
+
+    t4.join();
+    t5.join();
+    t6.join();
+    
+    // here, we also don't get 300000 because of data race
+    // some threads when resuming execution might not get latest information about counter
+    std::cout << counter << "\n";
+
+}
