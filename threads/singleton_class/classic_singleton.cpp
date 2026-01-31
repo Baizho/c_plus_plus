c31da4670098883a0a889f143920ce664cb8f9d1
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2026-01-31 21:43:20 +0900

    Implemented satic member function
    Finished implementing the Static member function to obtain the Singleton object

---
+++ threads/singleton_class/classic_singleton.cpp
@@ -0,0 +1,9 @@
+#include "classic_singleton.h"
+
+// Static member function to obtain the Singleton object
+Singleton* Singleton::get_Singleton() {
+    if (single == nullptr) {
+        single = new Singleton();
+    }
+    return single;
+}
