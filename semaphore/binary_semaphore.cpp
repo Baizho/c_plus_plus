4cb32cd0ce524083a156306d34bb9496f4acea6c
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2026-02-05 21:29:42 +0900

    Implementing classic Semaphore and binary semaphores

---
+++ semaphore/binary_semaphore.cpp
@@ -0,0 +1,69 @@
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+class Semaphore {
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int counter{0};
+    const int max_count{1};
+public:
+    void release() {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "Adding one item" << std::endl;
+
+        if (counter < max_count) {
+            ++counter;
+			count();
+		}
+
+        cv.notify_all();
+    }
+
+    void acquire() {
+        std::unique_lock<std::mutex> lock(mtx);
+        std::cout << "Removing one item" << std::endl;
+
+        while (counter == 0) {
+            cv.wait(lock);
+        }
+
+        --counter;
+        count();
+    }
+
+    void count() const {
+        std::cout << "Value of counter: ";
+        std::cout << counter << std::endl;
+    }
+};
+
+
+int main() {
+    Semaphore sem;
+
+    auto insert = [&sem]() {
+        sem.release();
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    };
+
+    auto relinquish = [&sem] {
+        sem.acquire();
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    };
+
+    std::vector<std::thread> tasks;
+
+    for (int i = 0; i < 2; ++i)
+        tasks.push_back(std::thread(insert));
+    for (int i = 0; i < 4; ++i)
+        tasks.push_back(std::thread(relinquish));
+    for (int i = 0; i < 3; ++i)
+        tasks.push_back(std::thread(insert));
+    for (auto& task : tasks) {
+        task.join();
+    }
+}
